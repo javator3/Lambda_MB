394e5b4c2ad547d66dbdaf863437ea564ad99323
Marek Blachowski <marekb2006@gmail.com>
2019-02-09 10:41:11 +0100

    Stream "between"

--- src/pl/sda/lambda/Main.java
+++ src/pl/sda/lambda/Main.java
@@ -13,13 +13,13 @@ public class Main {
         AddInterface add = (a, b) -> a + b;
         add.calc(4, 2);
 
-        List<Integer> numbers = Arrays.asList(1, 2,3,4,5,6,7);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
 
-        for (Integer integer: numbers){
+        for (Integer integer : numbers) {
             System.out.println(integer);
         }
 
-        Consumer<Integer> integerConsumer = n -> System.out.println(n) ;
+        Consumer<Integer> integerConsumer = n -> System.out.println(n);
         numbers.forEach(integerConsumer);
 
         numbers.forEach(s -> System.out.println(s
@@ -29,15 +29,20 @@ public class Main {
         List<Movie> movies = Arrays.asList(
                 new Movie("Tytul",
                         "Jan",
-                        LocalDate.of(2012,02,12),
+                        LocalDate.of(2012, 02, 12),
                         60,
                         Arrays.asList("Michal", "Maciek")),
                 new Movie("Tytul2",
                         "Jan",
-                        LocalDate.of(2012,02,12),
+                        LocalDate.of(2012, 02, 12),
                         20,
-                        Arrays.asList("Michal", "Maciek"))
-        );
+                        Arrays.asList("Michal", "Maciek")),
+                new Movie("Tytul3",
+                        "Mariusz",
+                        LocalDate.of(2012, 02, 12),
+                        25,
+                        Arrays.asList("Zdzisiek", "Krzysiek"))
+                );
 
 
         List<String> titles = movies
@@ -59,15 +64,15 @@ public class Main {
                 .filter(f -> f.getTitle().length() > 5)
                 .collect(Collectors.toList());
 
-        movieList.forEach(p-> System.out.println(p.getTitle() + p.getPrice()));
+        movieList.forEach(p -> System.out.println(p.getTitle() + p.getPrice()));
         movieList.forEach(System.out::println);
 
         List<Movie> movieList1 = movies
                 .stream()
-                .filter(f->f.getPrice()>10)
-                .filter(f->f.getPrice()<30)
+                .filter(f -> f.getPrice() > 10)
+                .filter(f -> f.getPrice() < 30)
                 .collect(Collectors.toList());
-        movieList1.forEach(p-> System.out.println(p.getTitle()));
+        movieList1.forEach(p -> System.out.println(p.getTitle()));
 
 
     }
